4548ad0f28fea896369cc8d78d77d187c25c6f3d
Cassandra Lee <zhicheng1224@gmail.com>
2026-04-12 23:13:04 +1000

    updated feed.tsx

--- app/routes/feed.tsx
+++ app/routes/feed.tsx
@@ -3,8 +3,11 @@ import { FeedPage } from "~/feed/feed";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Mastodon posts" },
-    { name: "description", content: "Mastodon posts!" },
+    { title: "MasClient" },
+    {
+      name: "description",
+      content: "A web client for mastodon social network.",
+    },
   ];
 }
 
